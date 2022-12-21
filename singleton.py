c1e85027ff10ed4261d7ef0e5c6dcc263860c817
lquintanillab06 <lquintanillab.dev@gmail.com>
2022-12-20 23:24:48 -0600

    Singleton

--- singleton.py
+++ singleton.py
@@ -1,6 +1,32 @@
 
+class SingletonMeta(type):
+
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class Singleton(metaclass=SingletonMeta):
+    def some_business_logic(self):
+        print("Ejecutando codigo del singleton")
+        
+
 
 
 if __name__ == "__main__":
 
-    print("Corriendo el codigo de prueba")
+    # The client code.
+
+    s1 = Singleton()
+    s2 = Singleton()
+
+    if id(s1) == id(s2):
+        print("Singleton correcto, ambas variables contienen la misma instancia")
+    else:
+        print("Singleton fallo, variables contienen diferentes instancias ")
+  
