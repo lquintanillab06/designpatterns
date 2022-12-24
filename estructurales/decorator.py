c685b12f2e9f47eb2a69f61eae3480a7f21a7f52
Luis <lquintanillab@gmail.com>
2022-12-23 18:26:04 -0600

    Composite y Decorator Version 2

--- estructurales/decorator.py
+++ estructurales/decorator.py
@@ -1,5 +1,11 @@
 from abc import ABC, abstractmethod
 
+"""
+    El patron decorator no es lo mismo que la funcion decorador.
+    Este patron hace uso de una interface que define la estructura para el Producto y el Decorador,
+    La funcion del decorador es agregar funcionanlidad a la clase decorada. pero debe tener la mis
+"""
+
 class Elemento(ABC):
 
     @abstractmethod
@@ -11,6 +17,11 @@ class Producto(Elemento):
 
     def method(self):
         print("Ejecutando el metodo del producto")
+
+class ProductoDeco(Elemento):
+    
+    def method(self):
+        print("Ejecutando el metodo del producto deco")
 
 class Decorator(Elemento):
 
@@ -28,3 +39,8 @@ if __name__ == "__main__":
 
     deco = Decorator(prod)
     deco.method()
+
+    prod_deco = Producto()
+    deco2 = Decorator(prod_deco)
+    deco2.method()
+
